db1788b7c8e6a12af3a297f5bd4cf17b367f58f6
Taemin Lee <dlrgks88@naver.com>
2023-03-17 00:11:21 +0900

    RequestBody

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -91,7 +91,7 @@ public class RequestParamController {
 
 
     @ResponseBody
-    @RequestMapping("/model-attribute-v2")  
+    @RequestMapping("/model-attribute-v2")
     public String modelAttributeV2(HelloData helloData) {
         log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
         return "ok";
